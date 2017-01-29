718b4e6d20dce549feb531fad7c85f7012d61eec
vkua <Favorut2015@yandex.ua>
2017-01-29 15:25:16 +0200

    зробив норм анімацію

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 $('document').ready(function(){
     $('.angle').click(function(){
-       $('.section1').css({
-           'display':'none'
-       });
-        $('.section2').css({
-            'display':'block'
+        $('.section1').animate({
+            opacity:'0.5',
+            marginTop:'-49%',
+            'display':'none'
+        },500,function(){
+            $('.section2').css({
+                display:'block',
+                marginTop:'0%'
+            });               
         });
+
     });
 });
